728b47a5755cfeddcffc8b3cf081d3197da6ce40
Woramon Jamjod <jworamon@gmail.com>
2022-03-26 18:57:16 -0400

    updated seatgeek api call to query by lat lon

--- client/resources/seatgeek.js
+++ client/resources/seatgeek.js
@@ -1,10 +1,21 @@
 const axios = require('axios');
 import { SEAT_GEEK_KEY } from '@env';
 
-const getEventsFromSeatGeek = async (zipcode, maxRadius) => {
+// const getEventsFromSeatGeek = async (zipcode, maxRadius) => {
+//     try {
+//         const key = SEAT_GEEK_KEY;
+//         const { data } = await axios.get(`https://api.seatgeek.com/2/events?geoip=${zipcode}&range=${maxRadius}mi&per_page=10&client_id=${key}`);
+//         // console.log('EVENTS:', events);
+//         return data.events;
+//     } catch (err) {
+//         console.log('error: ', err);
+//     }
+// }
+
+const getEventsFromSeatGeek = async (lat, lon, maxRadius) => {
     try {
         const key = SEAT_GEEK_KEY;
-        const { data } = await axios.get(`https://api.seatgeek.com/2/events?geoip=${zipcode}&range=${maxRadius}mi&per_page=10&client_id=${key}`);
+        const { data } = await axios.get(`https://api.seatgeek.com/2/events?lat=${lat}&lon=${lon}&range=${maxRadius}mi&per_page=10&client_id=${key}`);
         // console.log('EVENTS:', events);
         return data.events;
     } catch (err) {
