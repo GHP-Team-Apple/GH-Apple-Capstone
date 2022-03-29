6e1236e10e0f06d515691755592d53fecfb5c4d1
Carol Wu <carolwuq@gmail.com>
2022-03-29 18:03:58 -0400

    cleaned friendChat db connection

--- client/components/FriendChat.js
+++ client/components/FriendChat.js
@@ -3,41 +3,36 @@
 import React, { useState, useEffect, useCallback } from "react";
 import { GiftedChat } from "react-native-gifted-chat";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { StyleSheet, TextInput, View, Button, Dimensions, Text, ScrollView } from "react-native";
-// import firebase from "firebase/compat/app";
-// import "firebase/compat/auth";
-// import "firebase/compat/firestore";
-import {db} from "../../firebase"
-import { collection, query,deleteDoc, doc, updateDoc, deleteField, onSnapshot, where } from "firebase/firestore";
-
-
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCrTUJ7X_1J7ZE5A_fIh-TiMmA6TgeyaKc",
-//   authDomain: "gh-apple-capstone-f8e82.firebaseapp.com",
-//   databaseURL: "https://gh-apple-capstone-f8e82-default-rtdb.firebaseio.com",
-//   projectId: "gh-apple-capstone-f8e82",
-//   storageBucket: "gh-apple-capstone-f8e82.appspot.com",
-//   messagingSenderId: "985007094941",
-//   appId: "1:985007094941:web:ea1cd8e1372908eecbe874",
-//   measurementId: "G-YZDTJ9BHY9",
-// };
-
-// if (firebase.apps.length === 0) {
-//   firebase.initializeApp(firebaseConfig);
-// }
-
-// export const db = firebase.firestore();
-
+import {
+  StyleSheet,
+  TextInput,
+  View,
+  Button,
+  Dimensions,
+  Text,
+  ScrollView,
+} from "react-native";
+import { db } from "../../firebase";
+import {
+  collection,
+  query,
+  deleteDoc,
+  doc,
+  updateDoc,
+  deleteField,
+  onSnapshot,
+  where,
+} from "firebase/firestore";
 
 export default function FriendChat(props) {
-  const [user, setUser] = useState({"_id":`${props.user.uid}`,"name":`${props.user.firstName}`});
-//   const [name, setName] = useState("");
+  const [user, setUser] = useState({
+    _id: `${props.user.uid}`,
+    name: `${props.user.firstName}`,
+  });
   const [messages, setMessages] = useState([]);
 
   function readUser(props) {
-    const user = {"_id":`${props.user.uid}`,"name":`${props.user.firstName}`};
-    console.log('user in the readUser=======', user)
+    const user = { _id: `${props.user.uid}`, name: `${props.user.firstName}` };
     if (user) {
       setUser(user);
     }
@@ -45,8 +40,11 @@ export default function FriendChat(props) {
 
   useEffect(() => {
     readUser(props);
-    const chatsRef = query(collection(db, "Chats"),where("user", "in", [user, props.chatUser]));
-    const unsubscribe = onSnapshot(chatsRef,(querySnapshot) => {
+    const chatsRef = query(
+      collection(db, "Chats"),
+      where("user", "in", [user, props.chatUser])
+    );
+    const unsubscribe = onSnapshot(chatsRef, (querySnapshot) => {
       const messagesFirestore = querySnapshot
         .docChanges()
         .filter(({ type }) => type === "added")
@@ -54,7 +52,7 @@ export default function FriendChat(props) {
           const message = doc.data();
           return { ...message, createdAt: message.createdAt.toDate() };
         })
-        .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+        .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime());
       appendMessages(messagesFirestore);
     });
     return () => unsubscribe();
@@ -69,21 +67,18 @@ export default function FriendChat(props) {
     [messages]
   );
 
-
   async function handleSend(messages) {
     const writes = messages.map((m) => chatsRef.add(m));
     await Promise.all(writes);
   }
 
-  console.log("props.chatUser", props.chatUser)
-  console.log("messages", messages.length)
   return (
     <View style={styles.container}>
-      <Button
+      {/* <Button
         title="Back"
         onPress={() => props.handleBack()}
         style={styles.button}
-        />
+        /> */}
       <GiftedChat messages={messages} user={user} onSend={handleSend} />
     </View>
   );
@@ -106,9 +101,9 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     borderColor: "gray",
   },
-  button: {
-      marginTop: 400,
-      justifyContent: "center",
-      alignItems:"stretch"
-  }
+  //   button: {
+  //       marginTop: 400,
+  //       justifyContent: "center",
+  //       alignItems:"stretch"
+  //   }
 });
