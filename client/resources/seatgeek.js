76af3db58988adc3b2c0554cb58a69f702de6213
Woramon Jamjod <jworamon@gmail.com>
2022-03-30 23:04:51 -0400

    Eventbrite files for db seeding

--- client/resources/seatgeek.js
+++ client/resources/seatgeek.js
@@ -1,21 +1,9 @@
 const axios = require('axios');
 import { SEAT_GEEK_KEY } from '@env';
 
-// const getEventsFromSeatGeek = async (zipcode, maxRadius) => {
-//     try {
-//         const key = SEAT_GEEK_KEY;
-//         const { data } = await axios.get(`https://api.seatgeek.com/2/events?geoip=${zipcode}&range=${maxRadius}mi&per_page=10&client_id=${key}`);
-//         // console.log('EVENTS:', events);
-//         return data.events;
-//     } catch (err) {
-//         console.log('error: ', err);
-//     }
-// }
-
 const getEventsFromSeatGeek = async (type, lat, lon, maxRadius) => {
     try {
         const key = SEAT_GEEK_KEY;
-        // console.log("=======", key)
         const { data } = await axios.get(`https://api.seatgeek.com/2/events?taxonomies.name=${type}&lat=${lat}&lon=${lon}&range=${maxRadius}mi&per_page=10&client_id=${key}`);
         
         // set filter for events happening within the next 10 days
