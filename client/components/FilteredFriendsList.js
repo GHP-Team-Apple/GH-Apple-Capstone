f73b5e680a7c5a0e318117e7b66955eb54ae9cfb
Woramon Jamjod <jworamon@gmail.com>
2022-04-02 16:47:53 -0400

    fixed friendmap callout

--- client/components/FilteredFriendsList.js
+++ client/components/FilteredFriendsList.js
@@ -37,7 +37,7 @@ const FilteredFriendsList = (props) => {
           }}
           image={image}
         >
-          <Callout onPress={() => handlePress(event)} style={styles.event}>
+          <Callout onPress={() => props.handlePress(event)} style={styles.event}>
             <Text>{event.name}</Text>
           </Callout>
         </Marker>
